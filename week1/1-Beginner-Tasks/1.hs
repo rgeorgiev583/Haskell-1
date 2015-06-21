90694109a5db2f23db201fd0aef471f1bbe50675
Radoslav Georgiev <rgeorgiev583@gmail.com>
2015-06-22 00:50:11 +0300

    style fix: corrected formatting issues

--- week1/1-Beginner-Tasks/1.hs
+++ week1/1-Beginner-Tasks/1.hs
@@ -21,12 +21,12 @@ isTriangle a b c = a > 0 && b > 0 && c > 0 && a + b > c && b + c > a && a + c > 
 perimeter :: RealFloat a => a -> a -> a -> a
 perimeter a b c
     | not (isTriangle a b c) = error "This is not a triangle!"
-    | otherwise = a + b + c
+    | otherwise              = a + b + c
 
 area :: RealFloat a => a -> a -> a -> a
 area a b c
     | not (isTriangle a b c) = error "This is not a triangle!"
-    | otherwise = sqrt (p * (p - a) * (p - b) * (p - c))
+    | otherwise              = sqrt (p * (p - a) * (p - b) * (p - c))
         where p = (perimeter a b c) / 2
 
 calculate :: RealFloat a => Char -> a -> a -> a
@@ -47,14 +47,14 @@ convert   _     _    _  = error "Unknown currency!"
 
 
 isTriangleList :: RealFloat a => [a] -> Bool
-isTriangleList [] = False
-isTriangleList [a] = False
-isTriangleList [a, b] = False
+isTriangleList []        = False
+isTriangleList [a]       = False
+isTriangleList [a, b]    = False
 isTriangleList [a, b, c] = a > 0 && b > 0 && c > 0 && a + b > c && b + c > a && a + c > b
 isTriangleList     _     = False
 
 perimeterList :: RealFloat a => [a] -> a
-perimeterList [] = 0
+perimeterList []       = 0
 perimeterList (x : xs) = x + perimeterList xs
 
 areaList :: RealFloat a => [a] -> a
@@ -64,80 +64,80 @@ areaList     _     = error "Unknown polygon type!"
 
 
 head' :: [a] -> a
-head' [] = error "Cannot get the head of an empty list!"
+head' []      = error "Cannot get the head of an empty list!"
 head' (x : _) = x
 
 tail' :: [a] -> [a]
-tail' [] = error "Cannot get the tail of an empty list!"
-tail' [x] = []
+tail' []       = error "Cannot get the tail of an empty list!"
+tail' [x]      = []
 tail' (_ : xs) = xs
 
 last' :: [a] -> a
-last' [] = error "Cannot get the last element of an empty list!"
-last' [x] = x
+last' []       = error "Cannot get the last element of an empty list!"
+last' [x]      = x
 last' (_ : xs) = last' xs
 
 double :: Num a => [a] -> [a]
-double [] = []
+double []       = []
 double (x : xs) = 2 * x : double xs
 
 mult :: Num a => a -> [a] -> [a]
-mult _ [] = []
+mult _ []       = []
 mult n (x : xs) = n * x : mult n xs
 
 nth :: Int -> [a] -> a
-nth _ [] = error "Cannot get the n-th element of an empty list!"
-nth 0 (x : _) = x
+nth _ []       = error "Cannot get the n-th element of an empty list!"
+nth 0 (x : _)  = x
 nth n (_ : xs) = nth (n - 1) xs
 
 member :: Eq a => a -> [a] -> Bool
 member _ [] = False
 member n (x : xs)
-    | n == x = True
+    | n == x    = True
     | otherwise = member n xs
 
 isFib :: Integral a => [a] -> Bool
-isFib [] = False
-isFib [_] = False
-isFib [_, _] = True
+isFib []               = False
+isFib [_]              = False
+isFib [_, _]           = True
 isFib (x : y : z : xs) = x + y == z && isFib (y : z : xs)
 
 sum' :: Num a => [a] -> a
-sum' [] = 0
+sum' []       = 0
 sum' (x : xs) = x + sum' xs
 
 product' :: Num a => [a] -> a
-product' [] = 1
+product' []       = 1
 product' (x : xs) = x * product' xs
 
 multLists :: Num a => [a] -> [a] -> [a]
-multLists [] [] = []
-multLists _ [] = []
-multLists [] _ = []
+multLists []       []       = []
+multLists _        []       = []
+multLists []       _        = []
 multLists (x : xs) (y : ys) = x * y : multLists xs ys
 
 number2string :: Int -> String
 number2string 0 = "0"
 number2string n
-    | n < 0 = "-" ++ number2string (-n)
+    | n < 0            = "-" ++ number2string (-n)
     | n >= 1 && n <= 9 = [charDigit n]
-    | otherwise = number2string (n `quot` 10) ++ [charDigit (n `mod` 10)]
+    | otherwise        = number2string (n `quot` 10) ++ [charDigit (n `mod` 10)]
         where charDigit n = chr (ord '0' + n)
 
 string2number :: String -> Int
-string2number "" = 0
+string2number ""         = 0
 string2number ('-' : cs) = -(string2number cs)
-string2number s = digitChar (last s) + 10 * string2number (init s)
+string2number s          = digitChar (last s) + 10 * string2number (init s)
     where digitChar c = ord c - ord '0'
 
 isValidId :: String -> Bool
 isValidId [y1, y2, m1, m2, d1, d2, r1, r2, r3, cd] = digitChar cd == ckdigit && d >= 1 && d <= 28 && m >= 1 && m <= 12 || d == 29 && (m == 1 || m >= 3 && m <= 12 || m == 2 && isLeapYear) || d == 30 && (m == 4 || m == 6 || m == 9 || m == 11) || d == 31 && (m == 1 || m == 3 || m == 5 || m == 7 || m == 8 || m == 10 || m == 12)
     where
         digitChar c = ord c - ord '0'
-        cksum = (2 * digitChar y1 + 4 * digitChar y2 +
-                 8 * digitChar m1 + 5 * digitChar m2 +
+        cksum = (2  * digitChar y1 + 4 * digitChar y2 +
+                 8  * digitChar m1 + 5 * digitChar m2 +
                  10 * digitChar d1 + 9 * digitChar d2 +
-                 7 * digitChar r1 + 3 * digitChar r2 + 6 * digitChar r3) `mod` 11
+                 7  * digitChar r1 + 3 * digitChar r2 + 6 * digitChar r3) `mod` 11
         ckdigit = if cksum < 10 then cksum else 0
         yr = digitChar y1 * 10 + digitChar y2
         mr = digitChar m1 * 10 + digitChar m2
@@ -155,18 +155,18 @@ isValidId _ = False
 
 whatZodiacSignIs :: String -> String
 whatZodiacSignIs [_, _, m1, m2, d1, d2, _, _, _, _]
-    | d >= 21 && d <= 31 && m == 3 || d >= 1 && d <= 20 && m == 4 = "Aries"
-    | d >= 21 && d <= 30 && m == 4 || d >= 1 && d <= 21 && m == 5 = "Taurus"
-    | d >= 22 && d <= 31 && m == 5 || d >= 1 && d <= 21 && m == 6 = "Gemini"
-    | d >= 22 && d <= 30 && m == 6 || d >= 1 && d <= 22 && m == 7 = "Cancer"
-    | d >= 23 && d <= 31 && m == 7 || d >= 1 && d <= 22 && m == 8 = "Leo"
-    | d >= 23 && d <= 31 && m == 8 || d >= 1 && d <= 23 && m == 9 = "Virgo"
-    | d >= 24 && d <= 30 && m == 9 || d >= 1 && d <= 23 && m == 10 = "Libra"
+    | d >= 21 && d <= 31 && m ==  3 || d >= 1 && d <= 20 && m ==  4 = "Aries"
+    | d >= 21 && d <= 30 && m ==  4 || d >= 1 && d <= 21 && m ==  5 = "Taurus"
+    | d >= 22 && d <= 31 && m ==  5 || d >= 1 && d <= 21 && m ==  6 = "Gemini"
+    | d >= 22 && d <= 30 && m ==  6 || d >= 1 && d <= 22 && m ==  7 = "Cancer"
+    | d >= 23 && d <= 31 && m ==  7 || d >= 1 && d <= 22 && m ==  8 = "Leo"
+    | d >= 23 && d <= 31 && m ==  8 || d >= 1 && d <= 23 && m ==  9 = "Virgo"
+    | d >= 24 && d <= 30 && m ==  9 || d >= 1 && d <= 23 && m == 10 = "Libra"
     | d >= 24 && d <= 31 && m == 10 || d >= 1 && d <= 22 && m == 11 = "Scorpio"
     | d >= 23 && d <= 30 && m == 11 || d >= 1 && d <= 21 && m == 12 = "Sagittarius"
-    | d >= 22 && d <= 31 && m == 12 || d >= 1 && d <= 20 && m == 1 = "Capricorn"
-    | d >= 21 && d <= 31 && m == 1 || d >= 1 && d <= 19 && m == 2 = "Aquarius"
-    | d >= 20 && d <= 29 && m == 2 || d >= 1 && d <= 20 && m == 3 = "Pisces"
+    | d >= 22 && d <= 31 && m == 12 || d >= 1 && d <= 20 && m ==  1 = "Capricorn"
+    | d >= 21 && d <= 31 && m ==  1 || d >= 1 && d <= 19 && m ==  2 = "Aquarius"
+    | d >= 20 && d <= 29 && m ==  2 || d >= 1 && d <= 20 && m ==  3 = "Pisces"
     | otherwise = error "Invalid date: there is no such month/day number!"
         where
             digitChar c = ord c - ord '0'
