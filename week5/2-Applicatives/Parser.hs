46b1fc8f4b3675652719f09332a7be4ced890563
Radoslav Georgiev <rgeorgiev583@gmail.com>
2015-07-21 21:54:05 +0300

    Revert "reimplemented satisfy using fmap"
    This reverts commit cb28f155c5bb724cd00000da2770cb5190b0da77.

--- week5/2-Applicatives/Parser.hs
+++ week5/2-Applicatives/Parser.hs
@@ -30,7 +30,10 @@ instance Applicative Parser where
                                              Nothing      -> Nothing
 
 satisfy :: (Char -> Bool) -> Parser Char
-satisfy f = fmap (\x -> if f x then Just (x, xs) else Nothing) first
+satisfy f = Parser g
+  where g :: String -> Maybe (Char, String)
+        g (x : xs) = if f x then Just (x, xs) else Nothing
+        g    _     = Nothing
 
 char :: Char -> Parser Char
 char c = satisfy (== c)
