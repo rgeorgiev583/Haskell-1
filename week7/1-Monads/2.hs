2d329b889b5df5d7e0e030a69c868ce6c739b691
Radoslav Georgiev <rgeorgiev583@gmail.com>
2015-07-31 21:15:43 +0300

    added implementation of monadic sequence

--- week7/1-Monads/2.hs
+++ week7/1-Monads/2.hs
@@ -2,3 +2,9 @@
 
 join :: Monad m => m (m a) -> m a
 join x = x >>= id
+
+sequenceM :: Monad m => [m a] -> m [a]
+--(>>=) :: m a -> (a -> m [a]) -> m [a]
+--sequenceM l = foldl (\a x -> a >>= (\y -> return y +)) l
+sequenceM (x : xs) = x >>= \y -> (sequenceM xs) >>= \l -> return (y : l)
+sequenceM    _     = return [] 
