7e8259d5b52c761e64c82f9355e883dbbab31572
Radoslav Georgiev <rgeorgiev583@gmail.com>
2015-07-31 19:38:00 +0300

    fix: added default instances of Functor and Applicative for Maybe' and List (as required by the new version of GHC)

--- week7/1-Monads/monads.hs
+++ week7/1-Monads/monads.hs
@@ -1,7 +1,15 @@
 import List
+import Control.Monad
 
 data Maybe' a = Nothing' | Just' a
 --data (->) a = 
+
+instance Functor Maybe' where
+  fmap = liftM
+
+instance Applicative Maybe' where
+  pure  = return
+  (<*>) = ap
 
 instance Monad Maybe' where
   --return :: a -> m a
@@ -10,6 +18,13 @@ instance Monad Maybe' where
   --(>>=) :: m a -> (a -> m b) -> m b
   Nothing' >>= _ = Nothing'
   Just' x  >>= f = f x
+
+instance Functor List where
+  fmap = liftM
+
+instance Applicative List where
+  pure  = return
+  (<*>) = ap
 
 instance Monad List where
   --return :: a -> List a
