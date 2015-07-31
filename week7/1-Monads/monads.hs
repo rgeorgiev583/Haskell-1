e476fa025f2413348ffdce071d328ee10eb077be
Radoslav Georgiev <rgeorgiev583@gmail.com>
2015-07-31 19:02:45 +0300

    added implementations of monadic functions

---
+++ week7/1-Monads/monads.hs
@@ -0,0 +1,20 @@
+import List
+
+data Maybe' a = Nothing' | Just' a
+--data (->) a = 
+
+instance Monad Maybe' where
+  --return :: a -> m a
+  return x = Just' x
+
+  --(>>=) :: m a -> (a -> m b) -> m b
+  Nothing' >>= _ = Nothing'
+  Just' x  >>= f = f x
+
+instance Monad List where
+  --return :: a -> List a
+  return x = Cons x Empty
+
+  --(>>=) :: List a -> (a -> List b) -> List b
+  Empty     >>= _ = Empty
+  Cons x xs >>= f = f x >>= \y -> 
