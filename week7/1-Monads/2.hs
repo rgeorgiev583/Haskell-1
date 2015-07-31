4176947b8e555f96f2f15bc6b1dc25bc96949ae4
Radoslav Georgiev <rgeorgiev583@gmail.com>
2015-07-31 21:18:14 +0300

    style fix: dropped unnecessary parentheses around the first argument to the bind function

--- week7/1-Monads/2.hs
+++ week7/1-Monads/2.hs
@@ -6,5 +6,5 @@ join x = x >>= id
 sequenceM :: Monad m => [m a] -> m [a]
 --(>>=) :: m a -> (a -> m [a]) -> m [a]
 --sequenceM l = foldl (\a x -> a >>= (\y -> return y +)) l
-sequenceM (x : xs) = x >>= \y -> (sequenceM xs) >>= \l -> return (y : l)
+sequenceM (x : xs) = x >>= \y -> sequenceM xs >>= \l -> return (y : l)
 sequenceM    _     = return [] 
