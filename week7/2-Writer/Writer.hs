f6050ae80a34f78b06f4b2ae5503fc00d05b3261
Radoslav Georgiev <rgeorgiev583@gmail.com>
2015-08-04 17:08:36 +0300

    added incomplete instance of Monad for Writer

--- week7/2-Writer/Writer.hs
+++ week7/2-Writer/Writer.hs
@@ -9,3 +9,10 @@ mapWriter f writer = Writer { runWriter = result }
 
 tell :: w -> Writer w ()
 tell x = Writer { runWriter = ((), x) }
+
+instance Monad (Writer w) where
+    --return :: (a, w) -> Writer w a
+    return x = Writer { runWriter = (x, y) }
+
+    --(>>=) :: Writer w a -> (a -> Writer w b) -> Writer w b
+    writer >>= f = f (fst (runWriter writer))
