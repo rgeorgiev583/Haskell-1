2a63e468f3cc409857a39ce24b7a0066f3f8b74d
Radoslav Georgiev <rgeorgiev583@gmail.com>
2015-08-04 19:07:20 +0300

    finished Writer implementation

--- week7/2-Writer/Writer.hs
+++ week7/2-Writer/Writer.hs
@@ -1,18 +1,26 @@
+import Control.Monad
+
 newtype Writer w a = Writer { runWriter :: (a, w) }
 
 execWriter :: Writer w a -> w
 execWriter = snd . runWriter
 
 mapWriter :: ((a, w) -> (b, w')) -> Writer w a -> Writer w' b
-mapWriter f writer = Writer { runWriter = result }
-    where result = f (runWriter writer)
+mapWriter f writer = Writer { runWriter = f $ runWriter writer }
 
 tell :: w -> Writer w ()
 tell x = Writer { runWriter = ((), x) }
 
-instance Monad (Writer w) where
+instance Monoid w => Functor (Writer w) where
+    fmap = liftM
+
+instance Monoid w => Applicative (Writer w) where
+    pure  = return
+    (<*>) = ap
+
+instance Monoid w => Monad (Writer w) where
     --return :: (a, w) -> Writer w a
-    return x = Writer { runWriter = (x, y) }
+    return x = Writer { runWriter = (x, mempty) }
 
     --(>>=) :: Writer w a -> (a -> Writer w b) -> Writer w b
-    writer >>= f = f (fst (runWriter writer))
+    writer >>= f = f $ fst $ runWriter writer
