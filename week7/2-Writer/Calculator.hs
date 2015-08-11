ceef3c703db7aa029325776c685b92ccaed8d8b8
Radoslav Georgiev <rgeorgiev583@gmail.com>
2015-08-11 04:27:56 +0300

    finished RPN calculator

--- week7/2-Writer/Calculator.hs
+++ week7/2-Writer/Calculator.hs
@@ -1,10 +1,26 @@
 module Calculator where
 
-import Parser
-import qualified Data.Char as C (isDigit)
+data Oper  = Plus | Minus | Times | Divide | Mod
+data Token = Val Integer | Op Oper
 
-integer :: Parser Integer
-integer = read <$> (oneOrMore $ satisfy C.isDigit)
+parseToken :: String -> Token
+parseToken "+" = Op Plus
+parseToken "-" = Op Minus
+parseToken "*" = Op Times
+parseToken "/" = Op Divide
+parseToken "%" = Op Mod
+parseToken  s  = Val (read s :: Integer)
 
---calculate :: String -> Integer
+calculateRPN :: [Integer] -> [Token] -> Integer
+calculateRPN [result] [] = result
+calculateRPN stack (Val x : ts) = calculateRPN (x : stack) ts
+calculateRPN (ropnd : lopnd : stack) (Op op : ts) = case op of
+                                                      Plus   -> calculateRPN (lopnd   +   ropnd : stack) ts
+                                                      Minus  -> calculateRPN (lopnd   -   ropnd : stack) ts
+                                                      Times  -> calculateRPN (lopnd   *   ropnd : stack) ts
+                                                      Divide -> calculateRPN (lopnd `div` ropnd : stack) ts
+                                                      Mod    -> calculateRPN (lopnd `mod` ropnd : stack) ts
+calculateRPN _ _ = error "wrong RPN syntax"
 
+calculate :: String -> Integer
+calculate s = calculateRPN [] (map parseToken $ words s)
